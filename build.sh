c1e0b26861eb0b96b16f2f8897ea1402978b24ba
TNTSuperMan <tntsuperga@gmail.com>
2025-01-11 23:06:34 +0900

    tsを消す

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@ source ./emsdk/emsdk_env.sh
 mkdir -p bin
 em++ src/main.cpp -s WASM=1 -o bin/runtime.mjs
 em++ src/main.cpp -s WASM=1 -o bin/runtime.cjs
-em++ src/main.cpp -s WASM=1 -o bin/runtime.ts
